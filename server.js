34a6bacc62e316b3e47dee2be389d0b8ec7487e5
Dmitriy Proshutin <dmitry.proshutin@gmail.com>
2019-12-17 16:09:17 +0600

    Реализован POST запрос на /encode.Э

--- server.js
+++ server.js
@@ -3,35 +3,56 @@ const app = express();
 const PORT = 8000;
 const Vigenere = require('caesar-salad').Vigenere;
 
+app.use(express.json());
+
+app.post("/encode", (req, res) => {
+    console.log(req.body);
+    const plainText = req.body.message;
+    const key = req.body.password;
+    const cypherText = Vigenere.Cipher(key).crypt(plainText);
+    res.send({"encoded": cypherText});
+});
+
 app.get("/", (req, res) => {
     res.send(`
-    <div style="margin-left: 20px;">
+    <style>
+        div {
+            margin-left: 20px;
+        }
+        .small {
+            font-size: 14px;
+            color: blue;
+            margin-left: 20px;
+        }
+    </style>
+    <div>
         <h2>API для шифра Виженера</h2>
         <p>
         Сервер принимает данные POST-запросом в таком формате:
-            <p style="font-size: 14px;color: blue;margin-left: 20px;">
+            <p class="small">
             POST /encode HTTP/1.1<br>
             Content-Type: application/json<br>
             {"password": "testpassword", "message": "Test message"}
             </p>
         В ответ на такой запрос сервер должен ответить:
-            <p style="font-size: 14px;color: blue;margin-left: 20px;">
+            <p class="small">
             {"encoded": "mikm mwkooxh"}
             </p>
         Для расшифровки похожим способом, указываете пароль и зашифрованное сообщение, отправляете POST-запрос на /decode
-            <p style="font-size: 14px;color: blue;margin-left: 20px;">
+            <p class="small">
             POST /decode HTTP/1.1<br>
             Content-Type: application/json<br>
             {"password": "testpassword", "message": "mikm mwkooxh"}
             </p>
         Ответ:
-            <p style="font-size: 14px;color: blue;margin-left: 20px;">
+            <p class="small">
             {"decoded": "test message"}
             </p>
         </p>
     </div>
     `);
 });
+
 
 app.listen(PORT, () => {
     console.log(`Server started at ${PORT} port`);
